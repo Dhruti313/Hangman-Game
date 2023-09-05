ce940bd8f0ab7cc58b332e8195ab6d91caddeeef
Dhruti313 <124250238+Dhruti313@users.noreply.github.com>
2023-09-05 07:05:13 +0530

    final changes

--- src/HangmanWord.js
+++ src/HangmanWord.js
@@ -1,20 +1,31 @@
-export function HangmanWord(){
-    const word = "test"
-    return(
-        <div style={{
-            display :"flex",
-            gap:".25em",
-            fontSize :"6rem",
-            fontWeight :"bold",
-            textTransform :"uppercase",
-            fontFamily :"monospace",
-        }}
-        >
-            {word.split("").map((letter , index) =>(
-                <span style = {{borderBottom : ".1em solid black"}}>{letter}</span>
-            ))}
-
-
-        </div>
-    )
+export function HangmanWord({ guessedLetters, wordToGuess, reveal = false }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {wordToGuess.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility:
+                guessedLetters.includes(letter) || reveal
+                  ? "visible"
+                  : "hidden",
+              color:
+                !guessedLetters.includes(letter) && reveal ? "red" : "black",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
